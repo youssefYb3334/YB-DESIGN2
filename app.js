ad90c7dfdd099d152782bc5a30d95bc194f1b191
youssefYb3334 <youssefyb796@gmail.com>
2024-07-08 23:32:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+document.querySelector(".starts").style.setProperty("--width", window.innerHeight * 2 + "px");
+document.querySelector(".starts").style.setProperty("--height", window.innerHeight + "px");
